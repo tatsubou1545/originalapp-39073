7b870190b53b3d37b892c435b7e1ae25026cc991
tatsubou1545 <127082610+tatsubou1545@users.noreply.github.com>
2023-04-12 17:54:49 +0900

    投稿保存機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,25 @@
 class PostsController < ApplicationController
 
   def index
+    @post = Post.all
   end
   
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Item.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content, :category_id, :image).merge(user_id: current_user.id)
+  end
+
 end
