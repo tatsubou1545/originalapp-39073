2c29544a83975bbcfee4d25733588271697746ac
tatsubou1545 <127082610+tatsubou1545@users.noreply.github.com>
2023-04-14 11:08:20 +0900

    タイトル最大文字数変更に伴う単体テストコード修正

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :post do
-    title              { Faker::Book.title }
+    title              { Faker::Lorem.characters(number: 32)}
     content            { Faker::Lorem.sentence }
     category_id        { Category.find_by(name: '旅行').id }
     association :user
