107b820253d83addba80dbdfebce4779b32350d8
tatsubou1545 <127082610+tatsubou1545@users.noreply.github.com>
2023-04-17 13:36:42 +0900

    メッセージ画面表示

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -14,6 +14,10 @@ class RoomsController < ApplicationController
     end
   end
 
+  def show
+    @room = Room.find(params[:id])
+  end
+
   private
 
   def room_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'messages/index'
   devise_for :users
   get '/users', to: 'posts#index'
   root to: "posts#index"
