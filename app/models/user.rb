49fd9b75c20914f6cbb1b3d815fb77d289210d70
tatsubou1545 <127082610+tatsubou1545@users.noreply.github.com>
2023-04-20 19:02:22 +0900

    userモデル修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true, uniqueness: { case_sensitive: true }
-  validates :dog_number, allow_blank: true, format: { with: /\A[0-9]+\z/, message: "を数字で入力してください" }
   
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
     validates :password, format: { with: VALID_PASSWORD_REGEX }, on: :create
@@ -14,13 +13,5 @@ class User < ApplicationRecord
   has_many :room_users
   has_many :rooms, through: :room_users
   has_one_attached :image
-
-  def dog_age
-    today = Date.today
-    age = today.year - dog_birthday.year
-    if today.month < dog_birthday.month || (today.month == dog_birthday.month && today.day < dog_birthday.day)
-      age -= 1 # まだ誕生日を迎えていない
-    end
-    age
-  end
+  has_many :dog_profiles
 end
