e44232adc26dada35f32ffac2a8a23c62aa9bf6e
tatsubou1545 <127082610+tatsubou1545@users.noreply.github.com>
2023-04-21 23:38:24 +0900

    飼育数カラム削除

--- db/migrate/20230420094022_remove_columns_from_users.rb
+++ db/migrate/20230420094022_remove_columns_from_users.rb
@@ -1,6 +1,7 @@
 class RemoveColumnsFromUsers < ActiveRecord::Migration[6.0]
   def change
     remove_column :users, :dog_breed, :string
+    remove_column :users, :dog_number, :string
     remove_column :users, :dog_birthday, :date
     remove_column :users, :dog_profile, :text
   end
