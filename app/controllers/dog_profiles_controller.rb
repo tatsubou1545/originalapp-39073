fbd50f0fc48323b2054178600e3f445d32fa45d0
tatsubou1545 <127082610+tatsubou1545@users.noreply.github.com>
2023-04-21 21:16:31 +0900

    編集機能修正

--- app/controllers/dog_profiles_controller.rb
+++ app/controllers/dog_profiles_controller.rb
@@ -2,6 +2,7 @@ class DogProfilesController < ApplicationController
   before_action :move_to_index
   before_action :set_params, only: [:edit, :update, :destroy]
   before_action :compare_id, only: [:edit]
+  before_action :date_check, only: [:create, :update]
 
   def new
     @dog_profile = DogProfile.new
@@ -55,6 +56,37 @@ class DogProfilesController < ApplicationController
   def compare_id
     unless current_user.id == @dog_profile.user_id
       redirect_to user_path(current_user.id)
+    end
+  end
+
+  def date_check
+    year = params[:dog_profile]["dog_birthday(1i)"].to_i
+    month = params[:dog_profile]["dog_birthday(2i)"].to_i
+    day = params[:dog_profile]["dog_birthday(3i)"].to_i
+
+    if year.zero? || month.zero? || day.zero?
+      if (year.zero? && month.zero? && day.zero?) && @dog_profile.update(profile_params)
+        redirect_to user_path(current_user.id)
+      else
+        flash[:notice] = "存在しない日付が入力されています"
+        render :edit
+      end
+      return
+    end
+
+    begin
+      dog_birthday = Date.new(year, month, day)
+    rescue ArgumentError, TypeError
+      flash[:notice] = "存在しない日付が入力されています"
+      render :edit
+      return
+    end
+
+    dog_birthday = Date.new(year, month, day)
+    if dog_birthday > Date.today
+      flash[:notice] = "日付が未来日です"
+      render :edit
+      return
     end
   end
 end
