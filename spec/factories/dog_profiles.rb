70956a361f3a0b3d6acfee12fb2d101af48acdb6
tatsubou1545 <127082610+tatsubou1545@users.noreply.github.com>
2023-04-26 17:32:58 +0900

    userモデルの単体テスト追記

--- spec/factories/dog_profiles.rb
+++ spec/factories/dog_profiles.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :dog_profile do
-    
+    dog_breed             { 'トイプードル' }
+    dog_birthday          { Faker::Date.birthday }
+    dog_profile           { Faker::Lorem.sentence }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,9 +5,5 @@ FactoryBot.define do
     password              { Faker::Internet.password(min_length: 6) + '9z' }
     password_confirmation { password }
     user_profile          { Faker::Lorem.sentence }
-    dog_breed             { 'トイプードル' }
-    dog_number            { '2' }
-    dog_birthday          { Faker::Date.birthday }
-    dog_profile           { Faker::Lorem.sentence }
   end
 end
