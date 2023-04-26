e571af5d9d443954c4bf8c102fce9e297eb12371
tatsubou1545 <127082610+tatsubou1545@users.noreply.github.com>
2023-04-26 17:41:50 +0900

    messageモデルの単体テスト作成

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :message do
-    
+    content {Faker::Lorem.sentence}
+    association :user
+    association :room
+
+    after(:build) do |message|
+      message.image.attach(io: File.open('app/assets/images/Logo_black.png'), filename: 'Logo_black.png')
+    end
   end
 end
