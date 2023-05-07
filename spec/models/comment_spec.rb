35e9e1825f3f106a68a19a68dc937166dee77d56
tatsubou1545 <127082610+tatsubou1545@users.noreply.github.com>
2023-05-07 14:17:34 +0900

    単体テスト作成

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+
+  describe 'コメント投稿' do
+    context 'コメント投稿できる場合' do
+      it 'コメント内容が存在すれば登録できる' do
+        expect(@comment).to be_valid
+      end
+    end
+    context 'コメント投稿できない場合' do
+      it 'コメント内容が空では登録できない' do
+        @comment.text ='' 
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Text can't be blank")
+      end
+      it 'ユーザが紐づいていないと登録できない' do
+        @comment.user = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include('User must exist')
+      end
+      it '投稿内容が紐づいていないと登録できない' do
+        @comment.post = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include('Post must exist')
+      end
+    end
+  end
 end
