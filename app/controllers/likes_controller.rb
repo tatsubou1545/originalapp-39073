60a018e06103f73992ba2c1d2ba5fca0a70defdc
tatsubou1545 <127082610+tatsubou1545@users.noreply.github.com>
2023-05-14 20:55:41 +0900

    コントローラー内処理追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,20 @@
 class LikesController < ApplicationController
+  before_action :set_post
 
   def create
     like = current_user.likes.build(post_id: params[:post_id])
     like.save
-    redirect_to root_path
+    render 'create.js.erb'
   end
 
   def destroy
     like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
     like.destroy
-    redirect_to root_path
+    render 'destroy.js.erb'
+  end
+
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 
 end
